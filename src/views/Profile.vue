c6fb1174cc3f656de0bb7f60e19b9083b16f28bd
CatalinIuga <cataliniuga34@gmail.com>
2024-02-03 18:42:42 +0200

    Add loading spinner to profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -57,6 +57,7 @@ const profile = ref<ProfileResponse>({
   role: "",
   username: "",
 });
+const isLoading = ref(false);
 
 const usernameInput = ref("");
 const emailInput = ref("");
@@ -66,12 +67,15 @@ const newPasswordInput = ref("");
 
 onMounted(async () => {
   try {
+    isLoading.value = true;
     profile.value = await getProfile(token.value, userId.value);
     usernameInput.value = profile.value.username;
     emailInput.value = profile.value.email;
   } catch (error) {
     console.log(error);
   }
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  isLoading.value = false;
 });
 
 const editProfile = async () => {
@@ -152,7 +156,25 @@ const submissions = ref([
 </script>
 
 <template>
-  <div class="flex flex-1 grid-cols-7 flex-col gap-5 p-5 md:grid">
+  <div v-if="isLoading" class="flex w-full items-center justify-center">
+    <svg class="h-10 w-10 animate-spin text-primary" viewBox="0 0 24 24">
+      <circle
+        class="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      ></circle>
+      <path
+        class="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+  </div>
+
+  <div v-else class="flex flex-1 grid-cols-7 flex-col gap-5 p-5 md:grid">
     <!-- User data section -->
     <section class="col-span-2 flex flex-col gap-2">
       <Card>
